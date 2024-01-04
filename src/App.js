57997c4f4b9a18c0e5bd15557ebb38c62f3a96e0
Monet <monetpaul23@gmail.com>
2024-01-04 08:01:25 -0500

    finish  nav layout

--- src/App.js
+++ src/App.js
@@ -1,51 +1,19 @@
 import './index.css';
 //Comps
 import Nav from './Components/Nav';
-import Header from './Components/Header';
-import Highlights from './Components/Highlights';
-import Features from './Components/Features';
-import Latest from './Components/Latest';
-import Footer from './Components/Footer';
-//data
+
 import dataSeller from "./data-seller"
 import dataLast from "./data-lastest"
+import Landing from './Components/Landing';
 
 
 function App() {
   return (
     <div className="App">
-    <section id='landing'>
+    
       <Nav />
-    <Header />
-    </section>
-    <main>
-    <div id="both_wave">
-    <div id='top_wave' >
-    <div className='wave_container' >
-    <img  className='wave-img' src='topwave.svg'/>
-    </div>
-    </div>
-    <Highlights />
-    <div id='bottom_wave' >
-    <div className='wave_container' >
-    <img  src='bottom-svgbetter.svg'/>
-    </div>
-      </div>
-    </div>
-    <Features data={dataSeller} />
-    <Latest data={dataLast}/>
-    <section id="explore">
-    <div className="container">
-    <div className="big-row row_column">
-<h2>Explore more <span className="neon-blue">Cakes</span></h2>
-<a href='#features'>
-<button className='btn '> Browse Cakes</button>
-</a>
-</div>
-</div>
-    </section>
-    </main>
-    <Footer />
+      <Landing />
+    
     </div>
   );
 }
---
+++ src/Components/Landing.jsx
@@ -0,0 +1,27 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import React from "react"
+import undrawCake from "../assets/undraw_birthday_cake_re_bsw5.svg"
+
+
+const Landing = () => {
+    return (
+        <section id="landing">
+                <header>
+                    <div className="header__container">
+                        <div className="header__description">
+                            <h1>Welcome to Cake Delights, the ultimate destination for all your cake cravings in the USA.</h1>
+                            <h2>Our handcrafted cakes are the perfect addition to any celebration, and we take pride in being the best online cake shop.</h2>
+                            <a href="#features">
+                                <button className="btn">Browswe cakes</button>
+                            </a>
+                        </div>
+                        <figure  className="header__img--wrapper">
+                            <img src={undrawCake} alt="" />
+                        </figure>
+                    </div>
+                </header>
+
+        </section>
+    )
+    }
+    export default Landing;
