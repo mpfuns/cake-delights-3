6fef227edb16709bfafe079d1a72ba7c0544745f
Monet <monetpaul23@gmail.com>
2024-01-06 11:55:25 -0500

    edit explore

--- src/Components/explore.jsx
+++ src/Components/explore.jsx
@@ -6,7 +6,7 @@ const Explore = () => {
     <div className="container">
         <div className="row row__column">
             <h2>
-                Explore more  <span className="purple">Cakes</span>
+                Explore more <span className="purple">Cakes</span>
             </h2>
             <a href="/cakes">
             <button className='btn'>
